b3f0f522c8aff62546017a697f412547eaf44f83
码king <9806610+code-making@user.noreply.gitee.com>
2022-06-21 14:18:23 +0800

    deploy

---
+++ precache-manifest.04c6a722dbef34763750e94aa9c76c6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e6834d1714faef124c",
+    "url": "/tally-web/css/app.bca86d3a.css"
+  },
+  {
+    "revision": "7c9c3739e62562a04edd",
+    "url": "/tally-web/css/chunk-vendors.e96a0ae9.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/tally-web/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/tally-web/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "a8468e068b524706ad174c24beae2c95",
+    "url": "/tally-web/index.html"
+  },
+  {
+    "revision": "39e6834d1714faef124c",
+    "url": "/tally-web/js/app.ba23e0b7.js"
+  },
+  {
+    "revision": "7c9c3739e62562a04edd",
+    "url": "/tally-web/js/chunk-vendors.3a481a75.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/tally-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-web/robots.txt"
+  }
+]);
